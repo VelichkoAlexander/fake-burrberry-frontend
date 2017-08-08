491152bd4cc192f3788b11b5273e4aae74ddd9a7
Alexander Velichko <alvi@mail.ua>
2017-08-08 15:02:08 +0300

    feat: add Header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './images/logo.svg';
+export default () => {
+  return(
+    <header className="header">
+      <a className="header-link" href="/" >
+        <img className="header-logo" src={logo} alt="logo"/>
+      </a>
+    </header>
+  )
+};
+
