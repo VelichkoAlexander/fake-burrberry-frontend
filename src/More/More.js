4d307a7cec84b9231a6b2ba6c205c36d1a344279
Alexander Velichko <alvi@mail.ua>
2017-08-10 11:56:13 +0300

    fix: fix 'More' component variables

--- src/More/More.js
+++ src/More/More.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default (props) => {
+export default () => {
+  
   const Section = styled.section `
-   padding-right: 1rem;
-   padding-left: 1rem;
-   margin-bottom: 4rem;
+    padding-right: 1rem;
+    padding-left: 1rem;
+    margin-bottom: 4rem;
    
-   @media only screen and (min-width: 768px) {
-   display: none;
-   }
+    @media (min-width: 48rem) {
+      display: none;
+    }
   `;
   
   const Title = styled.h2 `
@@ -25,38 +26,36 @@ export default (props) => {
     padding-bottom: 0.5625rem;
     color: #171717;
     &::after {
-     content: '';
-     position: absolute;
-     left: 0;
-     bottom: 0;
-     width: 100%;
-     height: 0.0625rem;
-     background-color: #171717;
-     }
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 0.0625rem;
+      background-color: #171717;
+    }
   `;
   
   const Link = styled.a `
-   display: block;
-   margin: 0 0 1rem;
-   font-size: 0.875rem;
-   font-family: "Lora", serif;
-   font-style: italic;
-   line-height: 1.063rem;
-   text-decoration: none;
-   color: #171717;
-   &:last-child {
-     margin-bottom: 0;
-   }
-  
+    display: block;
+    margin: 0 0 1rem;
+    font-size: 0.875rem;
+    font-family: "Lora", serif;
+    font-style: italic;
+    line-height: 1.063rem;
+    text-decoration: none;
+    color: #171717;
+    &:last-child {
+      margin-bottom: 0;
+    }
   `;
   
-  const links = props.links.map((link, index) => {
-    return <Link href={link.href} key={index}>{link.title}</Link>;
-  });
   return (
     <Section>
-      <Title>{props.title}</Title>
-      {links}
+        <Title>More for you</Title>
+        <Link href="javascript:void(0);">Men’s Black Trench Coats</Link>
+        <Link href="javascript:void(0);">Men’s Short Trench Coats</Link>
+        <Link href="javascript:void(0);">Men’s Long Trench Coats</Link>
     </Section>
   )
 }
