4b4592b7d0d95acd1b4a333bd3af60ed77a10466
Alexander Velichko <alvi@mail.ua>
2017-08-10 19:17:54 +0300

    fix: styled App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
+import styled from 'styled-components';
 
 import Header from './Header';
 import Product from './Product';
@@ -8,8 +9,14 @@ import More from './More';
 import Footer from './Footer';
 
 export default () => {
+  const App = styled.div`
+    font-size: 16px;
+    font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  `;
   return (
-    <div className="App">
+    <App>
       <Helmet>
         <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
         <meta name="description" content="Long Cotton Gabardine Car Coat"/>
@@ -19,6 +26,6 @@ export default () => {
       <Suggest/>
       <More/>
       <Footer/>
-    </div>
+    </App>
   );
 }
