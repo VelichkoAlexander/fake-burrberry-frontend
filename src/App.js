60652345483539cfd1d3e5e08ae30d41530e18d5
Alexander Velichko <alvi@mail.ua>
2017-08-10 18:57:47 +0300

    fix: remove class from App.js, add prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Helmet} from 'react-helmet';
 
-import Header   from './Header';
-import Product  from './Product';
-import Suggest  from './Suggest';
-import More     from './More';
-import Footer   from './Footer';
+import Header from './Header';
+import Product from './Product';
+import Suggest from './Suggest';
+import More from './More';
+import Footer from './Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Helmet>
-          <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
-          <meta name="description" content="Long Cotton Gabardine Car Coat" />
-        </Helmet>
-        <Header/>
-        <Product/>
-        <Suggest/>
-        <More/>
-        <Footer/>
-      </div>
-    );
-  }
+export default () => {
+  return (
+    <div className="App">
+      <Helmet>
+        <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
+        <meta name="description" content="Long Cotton Gabardine Car Coat"/>
+      </Helmet>
+      <Header/>
+      <Product/>
+      <Suggest/>
+      <More/>
+      <Footer/>
+    </div>
+  );
 }
-
-export default App;
