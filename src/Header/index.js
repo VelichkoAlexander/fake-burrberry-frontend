b4a1ee3186d53ce8b5cc7edc7f282074e56cfabe
Alexander Velichko <alvi@mail.ua>
2017-08-12 11:26:10 +0300

    feat: add desktop version of header component

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,13 +9,24 @@ const Header = styled.header`
   padding-top: 1rem;
   padding-bottom: 1rem;
   border-bottom: solid 0.0625rem #c6c6c6;
-  @media only screen and (min-width: 768px) {
+  @media (min-width: 48rem) {
     padding-top: 1.5rem;
     padding-bottom: 1.5rem;
   }
+  @media (min-width: 62rem) {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    border-bottom: 0;
+  }
 `;
 
-const Logo = styled.img`display: block;`;
+const Logo = styled.img`
+  display: block;
+  @media (min-width: 62rem) {
+    width: 13.8125rem;
+    height: 1rem;
+  }
+`;
 
 export default () => {
   return (
