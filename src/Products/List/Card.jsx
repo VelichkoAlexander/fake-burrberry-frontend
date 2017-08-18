ab150fae244b643f9214d5f3fca43d573b8c8452
Alexander Velichko <alvi@mail.ua>
2017-08-18 22:54:02 +0300

    fix: fix svg favourite icon

--- src/Products/List/Card.jsx
+++ src/Products/List/Card.jsx
@@ -6,8 +6,6 @@ import PropTypes from 'prop-types';
 import { FormattedNumber } from 'react-intl';
 import { Link } from 'react-router-dom';
 
-import hart from '../../images/hart.svg';
-import hartBlack from '../../images/hart_black.svg';
 import { imageUrl, imageLg, imageMd, imageXs } from '../../common/ImageResponsive';
 
 const Wraper = styled.div`
@@ -49,8 +47,14 @@ const Favourite = styled.button`
     width: 0.875rem;
     height: 0.875rem;
     cursor: pointer;
-    background: url(${props => (props.fill ? `${hartBlack}` : `${hart}`)}) center no-repeat;
 `;
+
+const Path = styled.path `
+    fill: ${props => (props.fill ? '#000' : 'none')};
+    stroke: #000;
+    stroke-width: 1px;
+`;
+
 
 const Title = styled.a`
     margin: 0;
@@ -132,8 +136,16 @@ class Card extends Component {
           </Type>
           <Favourite
             onClick={this.handleFavourite}
-            fill={this.state.favourite}
           >
+            <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+              <Path
+                fill={this.state.favourite}
+                d="M1.724 7.342l5.3 5.71 5.32-5.732c1.403-1.512
+               1.403-3.995 0-5.507C10.94.3 8.656.3 7.233 1.813L7
+                2.06l-.23-.247C5.37.28 3.086.28 1.66 1.768.236 3.258.236 5.74
+                 1.618 7.275l.042.022.063.045z"
+              />
+            </svg>
             Favourite
           </Favourite>
         </Inner>
