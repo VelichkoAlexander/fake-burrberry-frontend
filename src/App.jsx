c02ba16f79a0a0c288af7ebddfdb33cb33beaefc
Alexander Velichko <alvi@mail.ua>
2017-08-31 00:52:49 +0300

    fix: fix header select remarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import ruLocaleData from 'react-intl/locale-data/ru';
 import enLocaleData from 'react-intl/locale-data/en';
@@ -14,63 +14,29 @@ import ScrollToTop from './common/ScrollToTop';
 
 addLocaleData([...ruLocaleData, ...enLocaleData]);
 
-// Detect preferred language
-const supportedLanguages = ['en', 'ru'];
-let language = 'en';
-if (navigator.languages) {
-  for (let i = 0; i < navigator.languages.length; i += 1) {
-    const lang = navigator.languages[i].toLowerCase();
-    if (supportedLanguages.includes(lang)) {
-      language = lang;
-      break;
-    }
-  }
-}
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = { local: language };
-    this.handelLocalChange = this.handelLocalChange.bind(this);
-  }
-
-  handelLocalChange(e) {
-    this.setState(() => ({ local: e }));
-  }
-
-  render() {
-    return (
-      <IntlProvider locale={this.state.local}>
-        <Router>
-          <ScrollToTop>
-            <div className="App">
-              <Helmet>
-                <title>Men’s Clothing | Burberry</title>
-                <meta
-                  name="description"
-                  content="Shop from the current men’s clothing collection.
+const App = () =>
+  (<IntlProvider locale="ru">
+    <Router>
+      <ScrollToTop>
+        <div className="App">
+          <Helmet>
+            <title>Men’s Clothing | Burberry</title>
+            <meta
+              name="description"
+              content="Shop from the current men’s clothing collection.
                        Sartorial suits, shirts and trousers feature as well as casual T-shirts,
                         polos and jeans."
-                />
-              </Helmet>
-              <Header
-                local={this.state.local}
-                hendelLocal={this.handelLocalChange}
-              />
+            />
+          </Helmet>
+          <Header />
 
-              <Route exact path="/" component={List} />
-              <Route exact path="/mens-clothing" component={List} />
-              <Route path="/mens-clothing/:categoryName/:id" component={Show} />
-              <Footer
-                local={this.state.local}
-                hendelLocal={this.handelLocalChange}
-              />
-            </div>
-          </ScrollToTop>
-        </Router>
-      </IntlProvider>
-    );
-  }
-}
+          <Route exact path="/" component={List} />
+          <Route exact path="/mens-clothing" component={List} />
+          <Route path="/mens-clothing/:categoryName/:id" component={Show} />
+          <Footer />
+        </div>
+      </ScrollToTop>
+    </Router>
+  </IntlProvider>);
 
 export default App;
--- src/Header/Select.jsx
+++ src/Header/Select.jsx
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 import arrow from '../images/arrow.svg';
 
 const SelectWrapper = styled.div`
   position: relative;
   display: inline-block;
-  z-index: 9;
-  &::after {
-    content: '';
-    position: absolute;
-    right: 0;
-    top: 50%;
-    width: 0.75rem;
-    height: 0.375rem;
-    transform: translateY(-50%);
-    background: url(${arrow}) center no-repeat;
-  }
 `;
 
 const CountrySelect = styled.select`
+  padding: 0;
+  display: block;
+  position: absolute;
+  top: 0;
+  border: none;
+  background-color: transparent;
+  font-size: 0.75rem;
+  font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  line-height: 1rem;
+  font-weight: 500;
+  color: #999999;
+  width: 100%;
+  appearance: none;
+  cursor: pointer;
+  opacity: 0;
+`;
+
+const CountryButton = styled.button`
   padding: 0;
   padding-right: 1.4375rem;
   display: block;
@@ -33,58 +39,48 @@ const CountrySelect = styled.select`
   font-weight: 500;
   color: #999999;
   white-space: nowrap;
-  appearance: none;
-  z-index: 10;
-  cursor: pointer;
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 50%;
+    width: 0.75rem;
+    height: 0.375rem;
+    transform: translateY(-50%);
+    background: url(${arrow}) center no-repeat;
+  }
 `;
 
-const countries = [
-  {
-    name: 'Russia (RUB)',
-    value: 'ru',
-  },
-  {
-    name: 'United Kingdom (£)',
-    value: 'en',
-  },
-];
+const countries = ['Russian Federation (₽)', 'United Kingdom (£)'];
 
 class Select extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: this.props.local };
+  constructor() {
+    super();
+    this.state = { country: 0 };
     this.handleChange = this.handleChange.bind(this);
   }
+
   handleChange(event) {
-    this.setState({ value: event.target.value });
-    this.props.hendelLocal(event.target.value);
+    this.setState({ country: event.target.value });
   }
+
   render() {
     return (
-      <div>
-        <SelectWrapper>
-          <CountrySelect value={this.state.value} onChange={this.handleChange}>
-            {countries.map((contry, index) =>
-              (<option
-                value={contry.value}
-                key={index.toString()}
-              >{`Shopping in: ${contry.name}`}</option>),
-            )}
-          </CountrySelect>
-        </SelectWrapper>
-      </div>
+      <SelectWrapper>
+        <CountryButton>
+          Shopping in: {countries[this.state.country]}
+        </CountryButton>
+        <CountrySelect onChange={this.handleChange}>
+          {countries.map((country, index) =>
+            (<option
+              value={index}
+              key={index.toString()}
+            >{`Shopping in: ${country}`}</option>),
+          )}
+        </CountrySelect>
+      </SelectWrapper>
     );
   }
 }
-
-Select.propTypes = {
-  hendelLocal: PropTypes.func,
-  local: PropTypes.string,
-};
-
-Select.defaultProps = {
-  hendelLocal: () => '',
-  local: 'en',
-};
 
 export default Select;
